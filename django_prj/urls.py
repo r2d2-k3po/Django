bbd1c1b2ac0d4a340db3e4f9f45c3d1c29f693d2
r2d2-k3po <r2d2factory3@gmail.com>
2022-06-11 20:52:49 +0900

    create project

--- django_prj/urls.py
+++ django_prj/urls.py
@@ -1,4 +1,4 @@
-"""do_it_django_prj URL Configuration
+"""django_prj URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
